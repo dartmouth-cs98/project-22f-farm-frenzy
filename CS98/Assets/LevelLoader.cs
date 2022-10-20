b1027a30e2503482f1cfdc9a443e6908ac290dba
avidixit27 <avidixit27@gmail.com>
2022-10-20 15:41:46 -0400

    Finished implementing a loading bar with a percentage that fills up as it loads

--- CS98/Assets/LevelLoader.cs
+++ CS98/Assets/LevelLoader.cs
@@ -1,9 +1,15 @@
 using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
 
 public class LevelLoader : MonoBehaviour
 {
+    public GameObject loadingScreen;
+    public Slider slider;
+    public TMP_Text progressText;
+
     public void LoadLevel(int sceneIndex) {
         
         StartCoroutine(LoadAsynchronously(sceneIndex));
@@ -13,9 +19,13 @@ public class LevelLoader : MonoBehaviour
     IEnumerator LoadAsynchronously (int sceneIndex) {
         AsyncOperation operation = SceneManager.LoadSceneAsync(sceneIndex);
 
+        loadingScreen.SetActive(true);
+
         while (!operation.isDone)
         {
-            Debug.Log(operation.progress);
+            float progress = Mathf.Clamp01(operation.progress / .9f);
+            slider.value = progress;
+            progressText.text = progress * 100f + "%";
 
             yield return null;
         }
