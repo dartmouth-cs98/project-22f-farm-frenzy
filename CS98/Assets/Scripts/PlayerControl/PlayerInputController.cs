5f9010e0a7024bad976842581a0fcea7e8879505
Yuchuan (Helen) Ma <yuchuan.ma.23@dartmouth.edu>
2022-10-21 16:09:35 -0400

    test1

---
+++ CS98/Assets/Scripts/PlayerControl/PlayerInputController.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using UnityEngine.InputSystem;
+
+public class PlayerInputController : MonoBehaviour
+{
+    public CameraMultiTarget cameraMultiTarget;
+
+    private List<GameObject> targets = new List<GameObject>();
+
+    // Start is called before the first frame updates
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+    
+    }
+
+    public void OnPlayerJoined(PlayerInput playerInput) {
+        GameObject[] players = GameObject.FindGameObjectsWithTag("Player");
+        Debug.Log("one") ;
+        foreach (GameObject p in players)
+        {
+            targets.Add(p);
+        }
+        cameraMultiTarget.SetTargets(targets.ToArray());
+    }
+
+}
