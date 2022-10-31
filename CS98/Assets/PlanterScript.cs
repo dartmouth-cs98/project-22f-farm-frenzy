c62f492ac96cc122065f01556956d7dc95e4873c
CancheMichael <michael.i.canche.23@dartmouth.edu>
2022-10-30 21:48:02 -0400

    Added some randomness to the growing, placeholder mechanic

--- CS98/Assets/PlanterScript.cs
+++ CS98/Assets/PlanterScript.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System.Linq;
-
 public class PlanterScript : MonoBehaviour
 {
 
@@ -77,7 +76,7 @@ public class PlanterScript : MonoBehaviour
     {
         if(EnteringObject.transform.parent == this.transform)
         {
-            GameObject newObj = Instantiate(growables[0]);
+            GameObject newObj = Instantiate(growables[Random.Range(0, growables.Length)]);
             newObj.transform.parent = EnteringObject.transform.parent;
             newObj.transform.position = EnteringObject.transform.position;
             if (EnteringObject) { Destroy(EnteringObject); }
