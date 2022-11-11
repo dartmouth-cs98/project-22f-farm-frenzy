0d867eba04a32e16425e80cb2aefada13fb5e98b
avidixit27 <avidixit27@gmail.com>
2022-11-11 16:45:44 -0500

    restored my score counter scene which got removed

---
+++ CS98/Assets/ScorePoint.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class ScorePoint : MonoBehaviour
+{
+
+    public TMP_Text scoreAmount;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        updateScoreUI();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter(Collision collision) {
+
+        if (collision.gameObject.tag == "Player") {
+            KeepScore.Score += 1;
+            Debug.Log("collision");
+            Destroy (gameObject);
+            updateScoreUI();
+
+        }
+
+    }
+
+    private void updateScoreUI() {
+        scoreAmount.text = KeepScore.Score.ToString("0");
+
+    }
+}
