303ed8d29ef3395a9d1b9cdc4443d0cd762ae2a3
CancheMichael <michael.i.canche.23@dartmouth.edu>
2022-11-11 15:11:15 -0500

    Revert "Revert "Merge branch 'updated-the-shop' into integration_testing_michael""
    This reverts commit 4aaf568bfea0916821dbe1a20a0b7d83c93aa9d1.

---
+++ CS98/Assets/HexTiles-Nature/Scripts/OLD/ModifyWaterUV.cs
@@ -0,0 +1,49 @@
+﻿#if UNITY_EDITOR
+using UnityEditor;
+using UnityEngine;
+
+namespace HexTileGrid
+{
+    [DisallowMultipleComponent]
+    [RequireComponent(typeof(MeshRenderer))]
+    [ExecuteInEditMode]
+    public class ModifyWaterUV : MonoBehaviour
+    {
+        private static readonly int waterRotateUV = Shader.PropertyToID("Vector1_5414065B");
+
+        private MeshRenderer waterMeshRenderer = null;
+        private GameObject rootObject = null;
+
+        private void OnEnable()
+        {
+            waterMeshRenderer = gameObject.GetComponent<MeshRenderer>();
+            rootObject = gameObject;
+
+            Material matInstance = new Material(waterMeshRenderer.sharedMaterial);
+            if (waterMeshRenderer.sharedMaterial.GetInstanceID() != matInstance.GetInstanceID())
+            {
+                waterMeshRenderer.sharedMaterial = matInstance;
+            }
+        }
+
+        private void Update()
+        {
+
+            if (EditorApplication.isPlaying || waterMeshRenderer == null || waterMeshRenderer.sharedMaterial == null)
+            {
+                return;
+            }
+
+            if (rootObject != null)
+            {
+                if (PrefabUtility.IsPartOfAnyPrefab(rootObject))
+                {
+                    rootObject = PrefabUtility.GetOutermostPrefabInstanceRoot(rootObject);
+                }
+
+                waterMeshRenderer.sharedMaterial.SetFloat(waterRotateUV, -rootObject.transform.rotation.eulerAngles.y + 70.0f);
+            }
+        }
+    }
+}
+#endif
