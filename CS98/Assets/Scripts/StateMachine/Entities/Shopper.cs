18809bffc5a07ae13035aa67dab6a615b516d377
Nathaniel Branscum <69632858+helenma0223234@users.noreply.github.com>
2023-01-11 22:31:23 -0500

    setups

---
+++ CS98/Assets/Scripts/StateMachine/Entities/Shopper.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using UnityEngine.AI;
+
+public class Shopper : MonoBehaviour
+{
+    private StateMachine _stateMachine;
+
+    private void Awake()
+    {
+        // navMesh for romaing
+        var navMeshAgent = GetComponent<NavMeshAgent>();
+        var animator = GetComponent<Animator>();
+        //var playerDetector = gameObject.AddComponent<PlayerDetector>();
+
+        _stateMachine = new StateMachine();
+
+        // state inits
+        var roam = new Roam(this, navMeshAgent, animator);
+
+        // transitions
+
+
+
+        // func bool checks
+        void At(IState to, IState from, Func<bool> condition) => _stateMachine.AddTransition(to, from, condition);
+        
+    }
+
+    // Update is called once per frame
+    private void Update() => _stateMachine.Tick();
+
+}
---
+++ CS98/Assets/Scripts/StateMachine/States/Roam.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+internal class Roam : IState
+{
+    private readonly Shopper _shopper;
+    private readonly NavMeshAgent _navMeshAgent;
+    private readonly Animator _animator;
+    private static readonly int Speed = Animator.StringToHash("Speed");
+
+    private Vector3 _lastPosition = Vector3.zero;
+    //public float TimeStuck;
+
+    public Roam(Shopper shopper, NavMeshAgent navMeshAgent, Animator animator)
+    {
+        _shopper = shopper;
+        _navMeshAgent = navMeshAgent;
+        _animator = animator;
+    }
+
+    public void Tick()
+    {
+        //if (Vector3.Distance(_shopper.transform.position, _lastPosition) <= 0f)
+        //    TimeStuck += Time.deltaTime;
+
+        _lastPosition = _shopper.transform.position;
+    }
+
+    public void OnEnter()
+    {
+        //TimeStuck = 0f;
+        _navMeshAgent.enabled = true;
+        //_navMeshAgent.SetDestination(_gatherer.Target.transform.position);
+        _animator.SetFloat(Speed, 1f);
+    }
+
+    public void OnExit()
+    {
+        _navMeshAgent.enabled = false;
+        _animator.SetFloat(Speed, 0f);
+    }
+
+}
