8e2e5785f860656d3ba51fc7678cf328c66ac142
Nathaniel Branscum <79519455+Coco-Huang1119@users.noreply.github.com>
2023-02-12 13:42:23 -0500

    create functionality that relates to the pool changing color
    create functionality that relates to the pool changing color

---
+++ CS98/Assets/ColorChanging.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorChanging : MonoBehaviour
+{
+
+    public ParticleSystem splash;
+    public Material body;
+    // Start is called before the first frame update
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "PlayerParent")
+        {
+            print("11111");
+            splash.Play();
+            Color changeColor = new Color(0.631f, 0.439f, 0.521f);
+            body.color = changeColor;
+        }
+    }
+}
