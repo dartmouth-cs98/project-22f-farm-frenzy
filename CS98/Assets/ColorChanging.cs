54dc177e3b3ff4210992489c84614562283fc3f0
CancheMichael <michael.i.canche.23@dartmouth.edu>
2023-02-15 13:04:59 -0500

    Updated color changing to be modular

--- CS98/Assets/ColorChanging.cs
+++ CS98/Assets/ColorChanging.cs
@@ -7,14 +7,19 @@ public class ColorChanging : MonoBehaviour
 
     public ParticleSystem splash;
     public Material body;
+    public Color changeColor = new Color(0.631f, 0.439f, 0.521f);
     // Start is called before the first frame update
     private void OnTriggerEnter(Collider other)
     {
-        if (other.tag == "PlayerParent")
+        if (other.tag == "Player")
         {
             print("11111");
             splash.Play();
-            Color changeColor = new Color(0.631f, 0.439f, 0.521f);
+            GameObject duck = other.transform.parent.parent.Find("duck").gameObject;
+            duck.transform.Find("body").GetComponent<Renderer>().material.color = changeColor;
+            duck.transform.Find("leftHand").gameObject.GetComponent<Renderer>().material.color = changeColor;
+            duck.transform.Find("rightHand").gameObject.GetComponent<Renderer>().material.color = changeColor;
+
             body.color = changeColor;
         }
     }
