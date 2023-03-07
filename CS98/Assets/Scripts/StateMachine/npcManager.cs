815a04047a2ff7a63ab575e452e8ae160b23042c
Kaihao Tian <69632858+helenma0223234@users.noreply.github.com>
2023-03-07 17:39:48 -0500

    bug fix

--- CS98/Assets/Scripts/StateMachine/npcManager.cs
+++ CS98/Assets/Scripts/StateMachine/npcManager.cs
@@ -15,35 +15,38 @@ public class npcManager : MonoBehaviour
     public float lifetimeOfEach = 5;    // life time of each shopper
     public float timer_destroy = 8;
     private GameObject shopper_created = null;
+    private bool has_shopper = false;
     public bool start = false;
     [SerializeField] private GameObject dieFX;
     private Vector3 offset = new Vector3(0f, -4.5f, 0f);
+    //[SerializeField] private static TriggerDialogue _triggerDialogue;
 
 
     void StartShopper()
     {
         shopper_created = Instantiate(shopper, new Vector3(pos_x, pos_y, pos_z), Quaternion.identity);
+        has_shopper = true;
     }
 
     void Update()
     {
         // TODO: add some form of check to check if game starts, then set start to true
-        //if (SceneManager.GetActiveScene().name == GameSceneName)
-        //{
-        //    start = true;
-        //}
+        if (TriggerDialogue.isGameStarted)
+        {
+            start = true;
+        }
         timer_create -= Time.deltaTime;
         // check count down for shopper creation
-        if (timer_create <= 0)
-        {
-            // uncomment below if checked game start or not
-        //if (timer_create <= 0 && start) {
+        //if (timer_create <= 0)
+        //{
+        // uncomment below if checked game start or not
+        if (timer_create <= 0 && start) {
             StartShopper();
             
             timer_create = interval;
         }
         // check within each shopper's life time
-        if (shopper_created != null)
+        if (has_shopper == true)
         {
             timer_destroy -= Time.deltaTime;
 
@@ -55,15 +58,11 @@ public class npcManager : MonoBehaviour
                 // play fx effect
                 shopper_created.GetComponent<dieDuck>().playFx();
                 GameObject fx = Instantiate(dieFX, shopper_created.GetComponent<dieDuck>().transform.position + offset, Quaternion.identity);
-                Destroy(fx, 1.3f);
-                if (timer_destroy <= 0)
-                {
+                Destroy(fx, 1.2f);
+
                 // refresh for next shopper
                     timer_destroy = lifetimeOfEach;
-                    shopper_created = null;
-                        
-                }
-                   
+                has_shopper = false; 
             }
         }
 
